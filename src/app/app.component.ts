6e7049fab7b5bd9e859e34b169d6288ca0a9cefa
Mave Hur <wlahd10@gmail.com>
2023-06-15 15:04:02 -0700

    Initial commit, Added movie-log project

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div>
+    <h1>{{title}}</h1>
+    <app-movies></app-movies>
+  </div>
+  `,
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title: string = 'first-angular';
+
+  getTitle(): string {
+    return this.title;
+  }
+}
