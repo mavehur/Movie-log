3aa087d1ffd607c8b28d32884d19ff61d30f2d4f
Mave Hur <wlahd10@gmail.com>
2023-06-15 16:48:13 -0700

    Implemented interface for movie

--- src/app/movies/movie-list.components.ts
+++ src/app/movies/movie-list.components.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { TitleStrategy } from "@angular/router";
+import { movie } from "./movie.model";
 
 @Component({
     selector: 'app-movies',
@@ -11,24 +12,26 @@ export class MovieListComponent {
     imgMargin: number = 2;
     isImgDisplayed: boolean = false;
     filterText = "Finding Nemo";
-    movies: any[] = [
+    movies: movie[] = [
         {
-            "movieId": 0,
-            "name": "Finding Nemo",
-            "director": "Andrew Stanton",
-            "releaseDate": "2003-May-30",
-            "cast": "Nemo",
-            "rating": 5,
-            "imageUrl": "assets/images/FindingNemo.jpeg"
+            movieId: 0,
+            name: "Finding Nemo",
+            director: "Andrew Stanton",
+            releaseDate: "2003-May-30",
+            cast: "Nemo",
+            rating: 5,
+            imageUrl: "assets/images/FindingNemo.jpeg",
+            price: 0
         }, 
         {
-            "movieId": 1,
-            "name": "Wallace and Gromit - A Grand Day Out",
-            "director": "Nick Park",
-            "releaseDate": "1989-May-18",
-            "cast": ["Wallace", "Gromit"],
-            "rating": 5,
-            "imageUrl": "assets/images/WallaceandGromit.jpg"
+            movieId: 1,
+            name: "Wallace and Gromit - A Grand Day Out",
+            director: "Nick Park",
+            releaseDate: "1989-May-18",
+            cast: "Wallace, Gromit",
+            rating: 5,
+            imageUrl: "assets/images/WallaceandGromit.jpg",
+            price: 0
         }
     ];
     public toggleImg(): void {
